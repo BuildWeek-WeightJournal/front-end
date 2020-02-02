ca3b9cc12d91247896c083218f1c6cf5a1cc93ac
Robert Stepanov <robertdstepanov@gmail.com>
2020-02-02 09:56:33 -0500

    Login Ready

--- front-end/src/Components/Login.js
+++ front-end/src/Components/Login.js
@@ -1,92 +1,81 @@
-import React, { useState } from 'react';
-import { axiosWithAuth } from './utils/axiosWithAuth';
-import { Link } from 'react-router-dom';
-
+import React, { useState } from "react";
+import { axiosWithAuth } from "./utils/axiosWithAuth";
+import { Link } from "react-router-dom";
 
 const Login = props => {
   const [username, setUsername] = useState({
-    username: ''
-  })
+    username: ""
+  });
 
   const [password, setPassword] = useState({
-    password: ''
-  })
+    password: ""
+  });
 
   const userHandleChange = e => {
     setUsername({
       username: e.target.value
-    })
-  }
+    });
+  };
 
   const passwordHandleChange = e => {
     setPassword({
       password: e.target.value
-    })
-  }
+    });
+  };
 
-  const mergedSignin = {...username, ...password}
+  const mergedSignin = { ...username, ...password };
 
   const login = e => {
     e.preventDefault();
 
     axiosWithAuth()
-    .post('/api/auth/login', mergedSignin)
+      .post("/api/auth/login", mergedSignin)
 
-    .then(res => {
-      localStorage.setItem('token', res.data.payload);
-      props.history.push('/protected')
-    })
-    .catch(err => console.log(err))
-  }
-  
- 
-  
-  
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/protected");
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <div>
       <div>
         <h1>Login Page</h1>
 
-        <form className="form-field" onSubmit={login}>
-                <label htmlFor="username">Username: </label>
-                <br />
-                <input
-                  className="field-input"
-                  id="username"
-                  type="username"
-                  name="username"
-                  value={username.username}
-                  onChange={userHandleChange}
-                />
-                <br />
-                {/* <ErrorMessage name="username" component="div" /> */}
-                <label htmlFor="password">Password: </label>
-                <br />
-                <input
-                  className="field-input"
-                  id="password"
-                  type="password"
-                  name="password"
-                  value={password.password}
-                  onChange={passwordHandleChange}
-                />
-                <br />
-                <br />
-                {/* <ErrorMessage name="password" component="div" /> */}
-                <button type="submit" 
-                // disabled={isSubmitting}
-                >
-                  Sign In
-                </button>
-                <p>or</p>
-                <Link to="/signup"><button>
-                Sign Up Here
-                </button></Link>
-              </form>
-        
+        <form onSubmit={login}>
+          <label htmlFor="username">Username: </label>
+          <br />
+          <input
+            id="username"
+            type="username"
+            name="username"
+            value={username.username}
+            onChange={userHandleChange}
+          />
+          <br />
+
+          <label htmlFor="password">Password: </label>
+          <br />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={password.password}
+            onChange={passwordHandleChange}
+          />
+          <br />
+          <br />
+
+          <button type="submit">Sign In</button>
+          <p>or</p>
+          <Link to="/signup">
+            <button>Sign Up Here</button>
+          </Link>
+        </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
