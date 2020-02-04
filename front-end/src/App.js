e1bd65ec4b60de93f981c00ad5208446dda96692
Allen Do <ad@Allens-MacBook-Pro.local>
2020-02-04 05:11:44 -0800

    Finished form for adding workouts but still need to style. Removed extra yarn install files not located in primary directory for app. Still need to integrate router for workout form when logging in.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Private from "./Components/PrivateRoute";
-
-
+import WorkoutForm from "./Components/workoutform";
 import "./App.css";
 import SignUp from "./Components/SignUp";
 import Login from "./Components/Login";
@@ -16,6 +15,7 @@ function App() {
           <Route path="/login" component={Login} />
           <Route exact path='/signup' component={SignUp} />
           <Route component={Login}/>
+          <Route path="/workout" component={WorkoutForm}/>
         </Switch>
       </Router>
     </div>
