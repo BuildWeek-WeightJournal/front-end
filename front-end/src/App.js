1b8e2814c742be2169644e487d813483d40fd536
Kai <31229627+KaiHaskell@users.noreply.github.com>
2020-02-04 13:57:10 -0500

    Netlify

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Private from "./Components/PrivateRoute";
-import NavBar from "./Components/NavBar";
 
 import SideDrawer from "./Components/SideDrawer/SideDrawer";
 import BackDrop from "./Components/BackDrop/BackDrop";
 import WorkoutForm from "./Components/workoutform";
+import NavBar from "./Components/NavBar";
 
 import "./App.css";
 import SignUp from "./Components/SignUp";
