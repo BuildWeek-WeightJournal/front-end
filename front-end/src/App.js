9767672053ba2ead193e7d08399899d1f66ee519
Kai <31229627+KaiHaskell@users.noreply.github.com>
2020-02-04 13:38:40 -0500

    ready for deployment

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,29 +3,25 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Private from "./Components/PrivateRoute";
 import NavBar from "./Components/NavBar";
 
-import SideDrawer from "./components/SideDrawer/SideDrawer";
-import BackDrop from "./components/BackDrop/BackDrop";
+import SideDrawer from "./Components/SideDrawer/SideDrawer";
+import BackDrop from "./Components/BackDrop/BackDrop";
 import WorkoutForm from "./Components/workoutform";
-
 
 import "./App.css";
 import SignUp from "./Components/SignUp";
 import Login from "./Components/Login";
-import UserForm from "./Components/UserForm"
+import UserForm from "./Components/UserForm";
 function App() {
-
-    return(
+  return (
     <div className="App">
       <Router>
-          <NavBar />
+        <NavBar />
 
         <Switch>
-          
-
-          <UserForm/>
+          <UserForm />
           <SideDrawer />
           <BackDrop />
-          <SignUp/>
+          <SignUp />
 
           <Route path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
--- front-end/src/Components/UserForm.js
+++ front-end/src/Components/UserForm.js
@@ -1,55 +1,59 @@
 import React from "react";
-import {withFormik,  Form,  Field} from "formik";
-import  * as Yup from 'yup';
-const UserForm = ({values,errors,touched,isSubmitting})=> {
-return(
-<div>
-  <Form>
-    <label htmlFor="email">
-      Email:
-      <div>
-        {touched.email && errors.email && <p>{errors.email}</p>}
-        <Field id="email" type="email" name="email" placeholder="Email:" />
-      </div>
-    </label>
-    <label htmlFor="password">
-      Password:
-      <div>
-        {touched.password && errors.password && <p>{errors.password}</p>}
-        <Field
-          id="password"
-          type="password"
-          name="password"
-          placeholder="Password:"
-        />
-      </div>
-    </label>
-    <button disabled={isSubmitting}>Sign In</button>
-  </Form>
-</div>;
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+const UserForm = ({ values, errors, touched, isSubmitting }) => {
+  return (
+    <div>
+      <Form>
+        <label htmlFor="email">
+          Email:
+          <div>
+            {touched.email && errors.email && <p>{errors.email}</p>}
+            <Field id="email" type="email" name="email" placeholder="Email:" />
+          </div>
+        </label>
+        <label htmlFor="password">
+          Password:
+          <div>
+            {touched.password && errors.password && <p>{errors.password}</p>}
+            <Field
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Password:"
+            />
+          </div>
+        </label>
+        <button disabled={isSubmitting}>Sign In</button>
+      </Form>
+    </div>
   );
-}
+};
 const FormikUserForm = withFormik({
-mapPropsToValues({email, password}){
-  return{
-    email:email || "",
-    password:password || ""
+  mapPropsToValues({ email, password }) {
+    return {
+      email: email || "",
+      password: password || ""
+    };
+  },
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email("Email not valid")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(7, "Password must be 7 Characters")
+      .required("Pasword is required")
+  }),
+  handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+    setTimeout(() => {
+      if (values.email === "adeeboom311082@gmail.com") {
+        setErrors({ email: "That email is already taken" });
+      } else {
+        resetForm();
+      }
+      setSubmitting(false);
+    }, 2000);
+    console.log(values);
   }
-},
-validationSchema: Yup.object().shape({
-email: Yup.string().email('Email not valid').required('Email is required'),
-password: Yup.string().min(7, 'Password must be 7 Characters').required('Pasword is required')
-}),
-handleSubmit(values, {resetForm, setErrors, setSubmitting}){
-setTimeout(() =>{
-  if(values.email === 'adeeboom311082@gmail.com'){
-  setErrors({email: 'That email is already taken'})
-}else{
-  resetForm()
-}
-setSubmitting(false)
-}, 2000)
-  console.log(values)
-}
-})(UserForm)
+})(UserForm);
 export default FormikUserForm;
