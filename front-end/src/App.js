d19386a4b48e63ace4b8f0806fa81e80207c6fca
Kai <31229627+KaiHaskell@users.noreply.github.com>
2020-02-04 19:01:58 -0500

    Commented out UserForm

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,8 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Private from "./Components/PrivateRoute";
 
-
-import UpdateWorkout from './Components/UpdateWorkout';
+import UpdateWorkout from "./Components/UpdateWorkout";
 import SideDrawer from "./Components/SideDrawer/SideDrawer";
 import BackDrop from "./Components/BackDrop/BackDrop";
 import WorkoutForm from "./Components/workoutform";
@@ -12,7 +11,7 @@ import NavBar from "./Components/NavBar";
 import "./App.css";
 import SignUp from "./Components/SignUp";
 import Login from "./Components/Login";
-import UserForm from "./Components/UserForm";
+// import UserForm from "./Components/UserForm";
 
 function App() {
   return (
@@ -20,10 +19,12 @@ function App() {
       <Router>
         <Switch>
           <Private exact path="/protected/workout" component={WorkoutForm} />
-          <Route path ='/update_workout/:id' render = {props =>
-          (<UpdateWorkout {...props} />)}/>
+          <Route
+            path="/update_workout/:id"
+            render={props => <UpdateWorkout {...props} />}
+          />
           <Route path="/login" component={Login} />
-          <Route exact path="/signup" component={UserForm} />
+          {/* <Route exact path="/signup" component={UserForm} /> */}
           <Route component={Login} />
         </Switch>
       </Router>
