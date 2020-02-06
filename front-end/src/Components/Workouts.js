9873f1b5fd3515a7d42ed2c7849c2f87e8576b25
Kai <31229627+KaiHaskell@users.noreply.github.com>
2020-02-06 05:29:09 -0500

    Started bugfixing

--- front-end/src/Components/Workouts.js
+++ front-end/src/Components/Workouts.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-const Workouts = (props) => {
+//components
+import NavBar from "./NavBar";
 
+//redux
+import { editWorkout, fetchWorkouts } from "../actions/actions";
+import { connect } from "react-redux";
 
+const Workouts = props => {
+  const [workouts, setWorkouts] = useState([]);
+
+  const id = props.match.params.id;
+
+  useEffect(() => {
+    if (props.exerciseList === 0) {
+      props.fetchWorkouts(id);
+    }
+    setWorkouts(props.exerciseList);
+  }, [props, props.exerciseList]);
+
+  console.log(setWorkouts);
 
   return (
     <div>
+      <NavBar />
       <h1>My Workouts</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Workouts;
+const mapStateToProps = state => {
+  return {
+    exerciseList: state.exerciseList,
+    isFetching: state.isFetching
+  };
+};
+
+export default connect(mapStateToProps, { editWorkout, fetchWorkouts })(
+  Workouts
+);
--- front-end/src/Components/workoutform.js
+++ front-end/src/Components/workoutform.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import { useParams } from "react-router-dom";
 // import anime from './anime-master/lib/anime.es.js';
 
 import { withFormik, Form, Field } from "formik";
@@ -14,13 +14,8 @@ import {
   CardSubtitle,
   CardText
 } from "reactstrap";
-import{
-  InputForm,
-  InputField,
-  Button,
-} from "./styles";
+import { InputForm, InputField, Button } from "./styles";
 import styled from "styled-components";
-
 
 import NavBar from "./NavBar";
 
@@ -35,83 +30,83 @@ import BackDrop from "./BackDrop/BackDrop";
 //   translateX: 270
 // });
 
-const cardStyle={
+const cardStyle = {
   margin: "auto",
   padding: "25px",
   width: "300px",
   border: " 1px solid #00a35e",
-  borderradius: "1px",
+  borderradius: "1px"
 };
-const containerStyle={
+const containerStyle = {
   display: "flex",
   flexdirection: "row",
   margin: "auto",
-  width: "1300px",
+  width: "1300px"
 };
 
 const WorkoutForm = ({ values, errors, touched, status }) => {
-const [workout, setWorkout] = useState([{}]);
+  const [workout, setWorkout] = useState([{}]);
+
   useEffect(() => {
     console.log("status has changed", status);
-    status && setWorkout(workout => 
-    [...workout, status])
-},[status]);
-    return(
-        <div className="workout-form">
-            <InputForm>
-                <label htmlFor="name">
-                    Please enter your a name for your workout:
-                <Field id ="name" type="text" name="name" placeholder="name"/>
-                {touched.name && errors.name && (
+    status && setWorkout(workout => [...workout, status]);
+  }, [status]);
+  return (
+    <div className="workout-form">
+      <InputForm>
+        <label htmlFor="name">
+          Please enter your a name for your workout:
+          <Field id="name" type="text" name="name" placeholder="name" />
+          {touched.name && errors.name && (
             <p className="errors"> {errors.name} </p>
           )}
-                </label>
-                <InputField className="muscle" as="select" name="muscle">
-                    <option>Choose a muscle group</option>
-                    <option value="Chest">Chest</option>
-                    <option value="Biceps">Biceps</option>
-                    <option value="Triceps">Triceps</option>
-                    <option value="Trapezius">Trapezius</option>
-                    <option value="Deltoids">Deltoids</option>
-                    <option value="Shoulders">Shoulders</option>
-                    <option value="Abdominals">Abdominals</option>
-                    <option value="Gluteals">Gluteals</option>
-                    <option value="Thighs">Thighs</option>
-                    <option value="Calves">Calves</option>
-                </InputField>
-                <label htmlFor="sets">
-                <Field id ="sets" type="text" name="sets" placeholder="sets"/>
-                {touched.sets && errors.sets && (
+        </label>
+        <InputField className="muscle" as="select" name="muscle">
+          <option>Choose a muscle group</option>
+          <option value="Chest">Chest</option>
+          <option value="Biceps">Biceps</option>
+          <option value="Triceps">Triceps</option>
+          <option value="Trapezius">Trapezius</option>
+          <option value="Deltoids">Deltoids</option>
+          <option value="Shoulders">Shoulders</option>
+          <option value="Abdominals">Abdominals</option>
+          <option value="Gluteals">Gluteals</option>
+          <option value="Thighs">Thighs</option>
+          <option value="Calves">Calves</option>
+        </InputField>
+        <label htmlFor="sets">
+          <Field id="sets" type="text" name="sets" placeholder="sets" />
+          {touched.sets && errors.sets && (
             <p className="errors"> {errors.sets} </p>
           )}
-                </label>
-                <label htmlFor="reps">
-                <InputField id ="reps" type="text" name="reps" placeholder="reps"/>
-                {touched.reps && errors.reps && (
+        </label>
+        <label htmlFor="reps">
+          <InputField id="reps" type="text" name="reps" placeholder="reps" />
+          {touched.reps && errors.reps && (
             <p className="errors"> {errors.reps} </p>
           )}
-                </label>
-                <Field as="textarea" type="text" name="notes" placeholder="Notes" />
-                <Button type="submit">Add Workout</Button>
-            </InputForm>
-    <Container>
-    <Row>
-    <Col xs="12" sm="6" md="4" xl="3" style={containerStyle}> 
-  {workout.map(props => (
-      <Card style={cardStyle} key={props.id}>
-        <CardTitle>Name: {props.name}</CardTitle>
-        <CardSubtitle>Muscle: {props.muscle}</CardSubtitle>
-        <CardSubtitle>Sets: {props.sets}</CardSubtitle>
-        <CardSubtitle>Reps: {props.reps}</CardSubtitle>
-        <CardText >Notes: {props.notes}</CardText>
-        <button> Edit Workout</button>
-	   </Card>
-	))}
-    </Col>
-  </Row>
-  </Container>
-        </div>
-    )
+        </label>
+        <Field as="textarea" type="text" name="notes" placeholder="Notes" />
+        <Button type="submit">Add Workout</Button>
+      </InputForm>
+      <Container>
+        <Row>
+          <Col xs="12" sm="6" md="4" xl="3" style={containerStyle}>
+            {workout.map(props => (
+              <Card style={cardStyle} key={props.id}>
+                <CardTitle>Name: {props.name}</CardTitle>
+                <CardSubtitle>Muscle: {props.muscle}</CardSubtitle>
+                <CardSubtitle>Sets: {props.sets}</CardSubtitle>
+                <CardSubtitle>Reps: {props.reps}</CardSubtitle>
+                <CardText>Notes: {props.notes}</CardText>
+                <button> Edit Workout</button>
+              </Card>
+            ))}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 const FormikWorkoutForm = withFormik({
@@ -135,7 +130,10 @@ const FormikWorkoutForm = withFormik({
     console.log("submitting", values);
 
     axios
-      .post("https://reqres.in/api/users/", values)
+      .post(
+        `https://weightliftingjournal-buildweek.herokuapp.com/api/workouts/:id`,
+        values
+      )
       .then(res => {
         console.log("success", res);
         setStatus(res.data);
